50f19244580323a6401a9d691a602df0d18d9451
Petter Eriksson <periksson@splunk.com>
2013-11-04 12:33:28 -0800

    Made create-jar script nicers

--- create-jar.sh
+++ create-jar.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eu
 
-script_dir=$(dirname $0)
+script_dir=$(cd $(dirname $0) && pwd)
 libs="$script_dir"/lib
 classes="$script_dir"/classes
+build="$script_dir"/build
+jar="$build"/hunk-image-reader.jar
 
 mkdir -p "$classes"
 
@@ -10,4 +12,6 @@ javac -classpath "$libs/*" \
     -d "$classes" \
     `find "$script_dir"/src -name "*.java"`
 
-(cd "$classes" && jar cf ../hunk-image-reader.jar com)
+mkdir -p $build
+(cd "$classes" && jar cf $jar com)
+echo "Created jar: $jar"
