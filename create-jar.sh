aa3be6ba8926fc9bc6e27c185c3ba6acfd86aafb
Petter Eriksson <periksson@splunk.com>
2013-11-04 12:33:27 -0800

    Updates the create-jar script to depend on jars

--- create-jar.sh
+++ create-jar.sh
@@ -1,15 +1,12 @@
 #!/bin/bash -eu
 
 script_dir=$(dirname $0)
-hunk_dir=$1
-hunk_libs="$hunk_dir"/libs
+libs="$script_dir"/lib
 classes="$script_dir"/classes
 
 mkdir -p "$classes"
 
-(cd $hunk_dir && ant build-1.0)
-
-javac -classpath "$hunk_libs/common/*":"$hunk_libs/1.0/*":"$hunk_dir/classes/1.0" \
+javac -classpath "$libs/*" \
     -d "$classes" \
     `find "$script_dir"/src -name "*.java"`
 
